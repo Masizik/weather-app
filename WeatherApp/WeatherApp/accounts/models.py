78b51ad9d99ee0e36210231fd779fc33d533abad
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-03-12 15:57:52 +0200

    create model, views(only definiton) and urls of AppUser

--- WeatherApp/WeatherApp/accounts/models.py
+++ WeatherApp/WeatherApp/accounts/models.py
@@ -1,3 +1,52 @@
+from django.contrib.auth import models as auth_models
+from django.core import exceptions
+from django.core.validators import MinLengthValidator
 from django.db import models
 
+
+def only_letters(value):
+    for ch in value:
+        if not ch.isalpha():
+            raise exceptions.ValidationError('Please type only letters!')
+
+
+def only_digits(value):
+    if not value.isdigit():
+        raise exceptions.ValidationError('Please type only digits!')
+
+
+def is_adult(value):
+    if value < 18:
+        raise exceptions.ValidationError('You must be at least 18 year old!')
+
+
 # Create your models here.
+class AppUser(auth_models.AbstractUser):
+    USERNAME_MIN_LEN = 6
+    USERNAME_MAX_LEN = 18
+    FIRST_NAME_MIN_LEN = 6
+    FIRST_NAME_MAX_LEN = 18
+    LAST_NAME_MIN_LEN = 6
+    LAST_NAME_MAX_LEN = 18
+
+    user_name = models.CharField(max_length=USERNAME_MAX_LEN,
+                                 validators=(MinLengthValidator(USERNAME_MIN_LEN),),
+                                 blank=False,
+                                 null=False,
+                                 ),
+    first_name = models.CharField(max_length=FIRST_NAME_MAX_LEN,
+                                  validators=(MinLengthValidator(FIRST_NAME_MIN_LEN), only_letters),
+                                  blank=False,
+                                  null=False,
+                                  ),
+    last_name = models.CharField(max_length=LAST_NAME_MAX_LEN,
+                                 validators=(MinLengthValidator(LAST_NAME_MIN_LEN), only_letters),
+                                 blank=False,
+                                 null=False,
+                                 ),
+    age = models.IntegerField(validators=(is_adult,),
+                              blank=False,
+                              null=False,)
+    picture = models.ImageField(upload_to='accounts_photos/',
+                                blank=True,
+                                null=True,)
--- WeatherApp/WeatherApp/accounts/views.py
+++ WeatherApp/WeatherApp/accounts/views.py
@@ -1,3 +1,32 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth import views as auth_views
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic as views
 
 # Create your views here.
+UserModel = get_user_model()
+
+
+class SignInView(auth_views.LoginView):
+    template_name = 'accounts/login-page.html'
+
+
+class RegistrationView(views.CreateView):
+    template_name = 'accounts/registration-page.html'
+
+
+class UserDetailsView(views.DetailView):
+    template_name = 'accounts/user-detail-page.html'
+
+
+class UserEditView(views.UpdateView):
+    template_name = 'accounts/user-edit-page.html'
+
+
+class UserDeleteView(views.DeleteView):
+    template_name = 'accounts/user-delete-page.html'
+
+
+class SignOut(auth_views.LogoutView):
+    next_page = reverse_lazy('login')
