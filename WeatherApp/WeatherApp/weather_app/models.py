94695f3aa5ad832cb724d83dbfbd242969f99d82
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-03-14 15:06:43 +0200

    generate API key and add history model that contains all searching

--- WeatherApp/WeatherApp/weather_app/models.py
+++ WeatherApp/WeatherApp/weather_app/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
+UserModel = get_user_model()
+
+
+class HistoryModel(models.Model):
+    country_code = models.TextField(
+        blank=False,
+        null=False,
+    )
+    country_name = models.TextField(
+        blank=False,
+        null=False,
+    )
+    city_name = models.TextField(
+        blank=False,
+        null=False,
+    )
+    latitude = models.FloatField(
+        blank=False,
+        null=False,
+    )
+    longitude = models.FloatField(
+        blank=False,
+        null=False,
+    )
+    timezone = models.TextField(
+        blank=False,
+        null=False,
+    )
+    population = models.TextField(
+        blank=False,
+        null=False,
+    )
+    create_date = models.DateField(
+        auto_now_add=True,
+        null=False,
+        blank=True,
+    )
+    user = models.ForeignKey(
+        UserModel,
+        on_delete=models.CASCADE,
+    )
+
+
