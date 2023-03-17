e17db1132b88620d59f5019700c607abbfc211f9
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-03-17 15:20:28 +0200

    create edit template

--- WeatherApp/WeatherApp/accounts/forms.py
+++ WeatherApp/WeatherApp/accounts/forms.py
@@ -12,6 +12,46 @@ class UserEditForm(auth_forms.UserChangeForm):
         fields = '__all__'
         field_classes = {'username': auth_forms.UsernameField, }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-horizontal'
+        self.helper.form_id = 'editUser'
+        self.helper.form_method = 'post'
+        self.helper.form_action = reverse_lazy('user edit')
+        self.helper.field_class = 'form-control form-control-lg'
+        self.helper.label_class = 'form-label'
+        self.helper.layout = Layout(
+            Div(
+                Div(
+                    Div(
+                        Div(
+                            Div(
+                                HTML(
+                                    """<i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
+          <span class="h1 fw-bold mb-0">Logo</span>"""),
+                                Div(
+                                    Fieldset(
+
+                                        'Edit', 'id', 'first_name', 'last_name', 'email', 'picture'
+
+                                    ),
+                                    Div(
+                                        Submit('submit', 'Edit', css_class='btn btn-info btn-lg btn-block'),
+
+                                        css_class='pt-1 mb-4',
+                                    ),
+                                ),
+                                css_class='d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5'
+                            ), css_class='px-5 ms-xl-4'
+                        ), css_class='col-sm-6 text-black'
+                    ), css_class='row'
+                ), css_class='container-fluid'
+
+            ),
+
+        ),
+
 
 class UserCreateForm(auth_forms.UserCreationForm):
     class Meta:
@@ -23,6 +63,7 @@ class UserCreateForm(auth_forms.UserCreationForm):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_class = 'form-horizontal'
+        self.helper.form_id = 'CreateUser'
         self.helper.form_method = 'post'
         self.helper.form_action = reverse_lazy('register user')
         self.helper.field_class = 'form-control form-control-lg'
